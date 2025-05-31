6787f0a7e9fa5ca0164db6a2b49018d856d59d76
Loopitt852 <loopitt852@gmail.com>
2025-06-01 00:22:24 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyAB9lFYoAh7xKiGSx-XWXTy7eSClXJefKI",
   authDomain: "loopitt-852.firebaseapp.com",
   databaseURL: "https://loopitt-852-default-rtdb.firebaseio.com",
   projectId: "loopitt-852",
-  storageBucket: "loopitt-852.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "loopitt-852.firebasestorage.app",
+  messagingSenderId: "115110500844",
+  appId: "1:115110500844:web:f459b9f2ed6829badd48c9",
+  measurementId: "G-EPQ5BBKX42"
 };
 
 const app = initializeApp(firebaseConfig);
